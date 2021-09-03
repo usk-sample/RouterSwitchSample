3ec76de99a2a402c6e529b4d1616fb0d374573f2
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-09-03 09:39:51 +0900

    Initial Commit

---
+++ RouterSwitchSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RouterSwitchSample
+//
+//  Created by Yusuke Hasegawa on 2021/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RouterSwitchSample/RouterSwitchSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterSwitchSampleApp.swift
+//  RouterSwitchSample
+//
+//  Created by Yusuke Hasegawa on 2021/09/03.
+//
+
+import SwiftUI
+
+@main
+struct RouterSwitchSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
